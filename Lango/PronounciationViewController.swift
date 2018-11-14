1c0a36bcc63b46cc1d5289ebda5e483ddfb6a64c
Jerry <zxcjerrygo@gmail.com>
2018-11-14 15:52:30 -0600

    add some files

---
+++ Lango/PronounciationViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PronounciationViewController.swift
+//  Lango
+//
+//  Created by  jerry on 11/14/18.
+//  Copyright © 2018  jerry. All rights reserved.
+//
+
+import UIKit
+
+class PronounciationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
