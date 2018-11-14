a93afd0da3a9bb74196402ae6d0e672304e45f50
Jerry <zxcjerrygo@gmail.com>
2018-11-14 15:34:01 -0600

    first commit

---
+++ Lango/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Lango
+//
+//  Created by  jerry on 11/14/18.
+//  Copyright © 2018  jerry. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Lango/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Lango
+//
+//  Created by  jerry on 11/14/18.
+//  Copyright © 2018  jerry. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
